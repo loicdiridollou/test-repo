366fa1a56afc16b76592f20a7c49088314084358
Loic Diridollou <loic.diridollou@gmail.com>
2024-02-20 21:14:06 -0800

    Test

---
+++ lib/module_name/tests/test_new_file.py
@@ -0,0 +1,5 @@
+from module_name.new_file import function_one
+
+
+def test_function_one():
+    assert function_one() == "Test"
---
+++ lib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="module_name",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Description",
+    author="Author",
+    license="MIT",
+    package_dir={"": "."},
+)
